d7d444277e6ca8b6c07451a7198e2dcbada4f521
nbq <mail@netbit.me>
2015-08-25 03:01:55 -0400

    fixed websockets ubuntu requirement, added runhifi to ubuntu

--- ubuntu-compile-hifi.sh
+++ ubuntu-compile-hifi.sh
@@ -35,7 +35,7 @@ function doapt {
 apt-get install -y screen git build-essential cmake qt5-default qtscript5-dev libssl-dev qttools5-dev qttools5-dev-tools \
 qtmultimedia5-dev libqt5svg5-dev libqt5webkit5-dev libsdl2-dev libasound2 libxmu-dev libxi-dev freeglut3-dev \
 libasound2-dev libjack-jackd2-dev libxrandr-dev libqt5xmlpatterns5-dev libqt5xmlpatterns5 \
-libqt5xmlpatterns5-private-dev qml-module-qtquick-controls
+libqt5xmlpatterns5-private-dev qml-module-qtquick-controls libqt5websockets5-dev
 
 }
 
@@ -186,6 +186,7 @@ function handlerc {
 cat <<EOF > ~/.coalrc
 alias killhifi='bash <(curl -Ls https://raw.githubusercontent.com/nbq/hifi-compile-scripts/master/centos7-kill-hifi.sh)'
 alias compilehifi='bash <(curl -Ls https://raw.githubusercontent.com/nbq/hifi-compile-scripts/master/ubuntu-compile-hifi.sh)'
+alias runhifi='bash <(curl -Ls https://raw.githubusercontent.com/nbq/hifi-compile-scripts/master/ubuntu-run-hifi.sh)'
 EOF
 
 }
---
+++ ubuntu-run-hifi.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+function checkroot {
+  [ `whoami` = root ] || { echo "Please run as root"; exit 1; }
+}
+
+function killrunning {
+  pkill -9 -f "[d]omain-server" > /dev/null 2>&1
+  pkill -9 -f "[a]ssignment-client" > /dev/null 2>&1
+}
+
+#function runashifi {
+  # Everything here is run as the user hifi
+#  TIMESTAMP=$(date '+%F')
+#  HIFIDIR=/usr/local/hifi
+#  HIFIRUNDIR=$HIFIDIR/run
+#  HIFILOGDIR=$HIFIDIR/logs
+#  cd $HIFIRUNDIR
+#  ./domain-server &>> $HIFILOGDIR/domain-$TIMESTAMP.log&
+#  ./assignment-client -n 3 &>> $HIFILOGDIR/assignment-$TIMESTAMP.log&
+#}
+
+function runashifi {
+  # Everything here is run as the user hifi
+  #TIMESTAMP=$(date '+%F')
+  HIFIDIR=/home/hifi
+  HIFIRUNDIR=$HIFIDIR/run
+  #HIFILOGDIR=$HIFIDIR/logs
+  cd $HIFIRUNDIR
+  screen -h 1024 -dmS hifi ./domain-server
+  screen -h 1024 -dmS hifi ./assignment-client -n 3
+  #./domain-server &>> $HIFILOGDIR/domain-$TIMESTAMP.log&
+  #./assignment-client -n 3 &>> $HIFILOGDIR/assignment-$TIMESTAMP.log&
+}
+
+checkroot
+killrunning
+export -f runashifi
+su hifi -c "bash -c runashifi"
+exit 0
